5947a3ef5421bb034a4cb77f1eaa663c7d7c130f
Quan <hao.q.cvla@gmail.com>
2020-01-06 21:25:46 -0500

    Use Redux thunk middleware to deal with asynchronize into redux.

--- src/containers/App.js
+++ src/containers/App.js
@@ -6,41 +6,35 @@ import Scroll from '../components/Scroll';
 import ErrorBoundary from "../components/ErrorBoundary";
 import './App.css'
 
-import {setSearchField} from "../action";
+import {requestRobots, setSearchField} from "../action";
 
 const mapStateToProps = state => {
     return {
-        searchField: state.searchField
+        searchField: state.searchRobots.searchField,
+        robots: state.requestRobots.robots,
+        isPending: state.requestRobots.isPending,
+        error: state.requestRobots.error
     }
 }
 
 const mapDispatchToProps = dispatch => {
     return {
-        onSearchChange: (event) => dispatch(setSearchField(event.target.value))
+        onSearchChange: (event) => dispatch(setSearchField(event.target.value)),
+        onRequestRobots: () => dispatch(requestRobots())
     }
 }
 
 class App extends Component {
-    constructor() {
-        super()
-        this.state = {
-            robots: []
-        }
-    }
-
     componentDidMount() {
-        fetch('https://jsonplaceholder.typicode.com/users')
-            .then(response => response.json())
-            .then(users => this.setState({robots: users}))
+        this.props.onRequestRobots()
     }
 
     render() {
-        const {robots} = this.state
-        const {searchField, onSearchChange} = this.props
+        const {searchField, onSearchChange, robots, isPending} = this.props
         const filterRobots = robots.filter(robot => {
             return robot.name.toLowerCase().includes(searchField.toLowerCase())
         })
-        return !robots.length ?
+        return isPending ?
             <h1>Loading</h1> :
             (
                 <div className='tc'>
